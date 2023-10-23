86067e67ccef8f63831ed84aaa8349c526447837
Kershockr <tim.merino@live.com>
2023-10-22 21:21:51 -0400

    Added description and tileset pairs, added code for getting maps from server

--- Assets/Scripts/TileSystem/MapGenerator.cs
+++ Assets/Scripts/TileSystem/MapGenerator.cs
@@ -1,6 +1,10 @@
 using System;
+using System.Collections;
+using System.Text;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+
 using Random = UnityEngine.Random;
 
 namespace Editor.TileSystem
@@ -12,6 +16,8 @@ namespace Editor.TileSystem
 
         [Header("Setting your tileset. The name of a tile has to be number")]
         public List<GameObject> tilesPrefab;
+
+        private const string URL = "https://sleepy-waters-75825.herokuapp.com/https://aigame.engineering.nyu.edu/generate";
 
 
         private int[,] _mapArray;
@@ -77,7 +83,8 @@ namespace Editor.TileSystem
         public void OnGenerateMap(string prompts)
         {
             // TODO generate MapArray based on the prompts;
-            RandomArray();
+            StartCoroutine(SendRequest(prompts));
+            // RandomArray();
             LoadTiles();
         }
 
@@ -85,6 +92,62 @@ namespace Editor.TileSystem
         public void SetMapArray(int[,] array)
         {
             _mapArray = array;
+        }
+
+        public IEnumerator SendRequest(string prompt)
+        {
+            string json = JsonUtility.ToJson(new { prompt = prompt });
+            byte[] body = Encoding.UTF8.GetBytes(json);
+
+            using (UnityWebRequest www = new UnityWebRequest(URL, "POST"))
+            {
+                www.uploadHandler = (UploadHandler)new UploadHandlerRaw(body);
+                www.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+
+                www.SetRequestHeader("Content-Type", "application/json");
+                www.SetRequestHeader("x-requested-with", "XMLHttpRequest");
+                www.SetRequestHeader("origin", "https://crocbot-site.herokuapp.com/fixmap");
+
+                yield return www.SendWebRequest();
+
+                if (www.result == UnityWebRequest.Result.ConnectionError)
+                {
+                    Debug.Log(www.error);
+                }
+                else
+                {
+                    // Parse the returned data
+                    Debug.Log(www.downloadHandler.text);
+                    int[,] parsedArray = ParseStringToArray(www.downloadHandler.text);
+                    SetMapArray(parsedArray);
+                }
+            }
+        }
+
+        public int[,] ParseStringToArray(string jsonString)
+        {
+
+            // Split the string to get individual arrays
+            string[] rows = jsonString.Trim('[', ']').Split(new string[] { "], [" }, StringSplitOptions.None);
+
+            int[,] result = new int[10, 10];
+
+            for (int i = 0; i < rows.Length; i++)
+            {
+                // Strip any remaining opening or closing square brackets from the row
+                string cleanedRow = rows[i].Replace("[", "").Replace("]", "");
+                Debug.Log("Row " + (i + 1) + ": " + cleanedRow); // Printing each row
+
+                // For each row, split by comma
+                string[] values = cleanedRow.Split(',');
+
+                for (int j = 0; j < values.Length; j++)
+                {
+                    result[i, j] = int.Parse(values[j].Trim()); // Ensure no spaces before parsing
+                }
+            }
+
+            return result;
         }
     }
 }
--- Assets/Scripts/UI/UITypeWriter.cs
+++ Assets/Scripts/UI/UITypeWriter.cs
@@ -11,6 +11,113 @@ namespace UI
     {
         public TextMeshProUGUI dialogueText;
         public float fDelay = 0.1f;
+        public string[] levelDescriptions = new string[] 
+        {
+            "The ground looks unique here. Fetch me some!",
+            "My spaceship needs a stable landing. What can I use?",
+            "I heard there's liquid on this planet. Can I see?",
+            "The local vegetation looks appetizing!",
+            "Mmm... Earth's barriers. Get me some!",
+            "I love the way earthlings communicate. Fetch me some means!",
+            "Need some resting spots. Get me some comfortable places.",
+            "I've heard of your earthly treasures. Show me!",
+            "Help me camouflage my spaceship!",
+            "I need some textures for my space art!",
+            "Looking for unique earth patterns!",
+            "Construct me a pathway!",
+            "Find me some beautiful earthly colors!",
+            "I adore Earth's architecture. Fetch me some components!",
+            "Curious about your planet's surfaces!",
+            "Fetch me some items for my alien garden!",
+            "I wish to make a terrain collage!",
+            "Craft me an earthly aesthetic!",
+            "Build me a miniature earth landscape!",
+            "Design me an Earthly relaxation spot!",
+            "Craft me a pathway to your world's wonders!",
+            "Recreate me a piece of your homeland!",
+            "I've seen Earthly wonders from above. Show them up close!",
+            "I love Earth's natural formations!",
+            "Construct me a colorful set!",
+            "I need to send a message. Help me out!",
+            "Design me a serene Earthly spot!",
+            "Help me create an Earth maze!",
+            "Need to create a hideout spot. Help!",
+            "Design me a typical Earth courtyard!",
+            "Craft me a collection of your planet's symbols!",
+            "Want to experiment with textures. Fetch me some!",
+            "Show me the beauty of your flora!",
+            "I need to build a shelter. Help me out!",
+            "Recreate me an earthly oasis!",
+            "The colors of your planet amaze me! Show more!",
+            "I'm craving Earth's natural beauty!",
+            "I want to see more of Earth's geometric patterns!",
+            "Seeking some of Earth's unique mysteries!",
+            "Craft me a tiny Earth park!",
+            "I'm looking for earth's toughest elements!",
+            "Design me an Earth-style lounge area!",
+            "Construct me a nature trail!",
+            "I want to learn about Earth's textures!",
+            "I'm looking for places to hide treasures!",
+            "Give me some symbols of your world!",
+            "I wish to see Earth's favorite spots!",
+            "Help me design a pathway to beauty!",
+            "I'm curious about your typical outdoor designs!",
+            "Craft me a vibrant Earth scene!"
+        };
+
+        public int[][] tileLists = new int[][] 
+        {
+            new int[] {0, 1, 6},
+            new int[] {5, 7, 6},
+            new int[] {11, 1, 6},
+            new int[] {10, 2, 4},
+            new int[] {9, 7, 10},
+            new int[] {8, 14, 13},
+            new int[] {0, 6, 5},
+            new int[] {15, 8, 9},
+            new int[] {10, 0, 12},
+            new int[] {1, 11, 6},
+            new int[] {4, 3, 1},
+            new int[] {5, 0, 7},
+            new int[] {2, 11, 1},
+            new int[] {13, 14, 12},
+            new int[] {6, 5, 1},
+            new int[] {10, 3, 4},
+            new int[] {7, 11, 6},
+            new int[] {2, 5, 13},
+            new int[] {0, 10, 11},
+            new int[] {0, 2, 6},
+            new int[] {5, 9, 0},
+            new int[] {1, 10, 11},
+            new int[] {3, 12, 10},
+            new int[] {7, 10, 11},
+            new int[] {3, 2, 11},
+            new int[] {8, 9, 14},
+            new int[] {0, 11, 10},
+            new int[] {9, 7, 10},
+            new int[] {10, 7, 13},
+            new int[] {0, 8, 5},
+            new int[] {15, 10, 14},
+            new int[] {1, 6, 5},
+            new int[] {3, 2, 4},
+            new int[] {12, 14, 13},
+            new int[] {11, 6, 10},
+            new int[] {2, 1, 11},
+            new int[] {10, 11, 3},
+            new int[] {9, 14, 5},
+            new int[] {15, 8, 13},
+            new int[] {0, 10, 9},
+            new int[] {7, 9, 10},
+            new int[] {0, 12, 10},
+            new int[] {1, 11, 10},
+            new int[] {6, 7, 5},
+            new int[] {8, 0, 10},
+            new int[] {13, 14, 15},
+            new int[] {5, 10, 11},
+            new int[] {2, 5, 0},
+            new int[] {9, 0, 8},
+            new int[] {3, 11, 10},
+        };
 
         public static UITypeWriter Instance;
 
@@ -89,7 +196,17 @@ namespace UI
         // TODO Get the desired description
         public string GetDescription()
         {
-            return "I want a beautiful city.";
+            // Get a random index
+            System.Random rand = new System.Random();
+            int randomIndex = rand.Next(levelDescriptions.Length);
+
+            // Retrieve the description and tile list for the random index
+            string randomDescription = levelDescriptions[randomIndex];
+            int[] randomTileList = tileLists[randomIndex];
+
+
+            return randomDescription;
         }
+    
     }
 }
